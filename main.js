7f0f3e229ad1719c85ba900872bb7ec97e383a6e
TsybulskyAlexandr <TsybulskyAlexandr@gmail.com>
2018-07-28 14:48:48 +0300

    test
    test

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
-window.onload = function() {
-  var button_click = document.querySelector('#button_click');
-  var myElem = document.createElement("p");
-  function getDecimalNumber() {
-    var decNum = document.querySelector('#decimal_number');
-    var arr = [];
-    var val = decNum.value;
-    while (val != 0) {
-      var remain = val % 2;
-      val = Math.floor(val / 2);
-      arr.push(remain);
+window.addEventListener("DOMContentLoaded", function(event) {
+  document.forms['decimalToBinary'].onsubmit = function(){    
+    event.preventDefault();
+    document.getElementsByClassName('result_out')[0].innerHTML = "";
+    var number = document.getElementById("decimalNumber").value;      
+    if(number == '') return document.getElementsByClassName('result_out')[0].innerHTML = "Поле ввода не должно быть пустым";
+    if(!number.match(/^\d+$/)) return document.getElementsByClassName('result_out')[0].innerHTML = "Поле должно содержать только цифры";
+    var binaryNumber = '0';
+    var modulo = null;
+    var val = null;
+    while (number != 0) {      
+      modulo = number % 2;      
+      number = Math.floor(number / 2);
+      binaryNumber +=  modulo;      
     }
-    arr.reverse();
-    var str = arr.join("");
-    var result = +str;
-      myElem.innerHTML = result;
-      test.appendChild(myElem);
-    }
-  button_click.addEventListener("click", getDecimalNumber);
-}
+    binaryNumber = binaryNumber.split("").reverse().join("");
+    return document.getElementById("decimalToBinary").getElementsByClassName('result_out')[0].innerHTML = binaryNumber;
+  }
+});
