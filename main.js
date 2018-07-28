ab689bafcbba3a573d09845ace755a467a6b4ea1
Tsybulsky Alexandr <tsybulskyalexandr@gmail.com>
2018-07-28 20:04:51 +0300

    test
    test

--- main.js
+++ main.js
@@ -1,19 +1,30 @@
 window.addEventListener("DOMContentLoaded", function(event) {
   document.forms['decimalToBinary'].onsubmit = function(){    
-    event.preventDefault();
-    document.getElementsByClassName('result_out')[0].innerHTML = "";
-    var number = document.getElementById("decimalNumber").value;      
-    if(number == '') return document.getElementsByClassName('result_out')[0].innerHTML = "Поле ввода не должно быть пустым";
-    if(!number.match(/^\d+$/)) return document.getElementsByClassName('result_out')[0].innerHTML = "Поле должно содержать только цифры";
-    var binaryNumber = '0';
-    var modulo = null;
-    var val = null;
-    while (number != 0) {      
-      modulo = number % 2;      
-      number = Math.floor(number / 2);
-      binaryNumber +=  modulo;      
-    }
-    binaryNumber = binaryNumber.split("").reverse().join("");
-    return document.getElementById("decimalToBinary").getElementsByClassName('result_out')[0].innerHTML = binaryNumber;
+	event.preventDefault();  
+	resultOut('');
+	var number = document.getElementById("decimalNumber").value;      
+	if(number == '') return resultOut("Поле ввода не должно быть пустым");
+	if(!number.match(/^\d+$/)) return resultOut("Поле должно содержать только цифры");
+	var binaryNumber = '';
+	var modulo = null;
+	var val = null;
+	while (number !== 0) {
+	  modulo = number % 2;      
+	  number = Math.floor(number / 2);
+	  binaryNumber +=  modulo;
+	}
+	binaryNumber = binaryNumber.split("").reverse().join("");
+	return resultOut(binaryNumber);
+  }  
+  document.forms['binaryToDecimal'].onsubmit = function(){
+  	event.preventDefault();
+  	resultOut('');
+  	var number = document.getElementById("binaryNumber").value;
+	if(number == '') return resultOut("Поле ввода не должно быть пустым");
+	if(!number.match(/^\d+$/)) return resultOut("Поле должно содержать только цифры");
+	if(number < 0 || number > 1) return resultOut("Введите число в двоично представлении");
+  }
+  function resultOut(string) {
+	return document.getElementById('binaryToDecimal').getElementsByClassName('result_out')[0].innerHTML = string;
   }
 });
