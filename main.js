6d32ea28e4bdf9e10ec78f08635911635a2fbd90
Tsybulsky Alexandr <tsybulskyalexandr@gmail.com>
2018-08-03 08:42:00 +0300

    test
    test

--- main.js
+++ main.js
@@ -1,10 +1,4 @@
-window.addEventListener("DOMContentLoaded", function(event) {
-  /*document.forms['1decimalToBinary'].onsubmit = function(){    
-	event.preventDefault();  
-	resultOut('');
-	var number = document.getElementById("decimalNumber").value;      
-	if(number == '') return resultOut("Поле ввода не должно быть пустым");
-	if(!number.match(/^\d+$/)) return resultOut("Поле должно содержать только цифры");
+function decimalToBinary (_this, number){
 	var binaryNumber = '';
 	var modulo = null;
 	var val = null;
@@ -14,15 +8,10 @@ window.addEventListener("DOMContentLoaded", function(event) {
 	  binaryNumber +=  modulo;
 	}
 	binaryNumber = binaryNumber.split("").reverse().join("");
-	return resultOut(binaryNumber);
-  }  
-  document.forms['1binaryToDecimal'].onsubmit = function(){
-  	event.preventDefault();
-  	resultOut('');
-  	var number = document.getElementById("binaryNumber").value;
-	if(number == '') return resultOut("Поле ввода не должно быть пустым");
-	if(!number.match(/^\d+$/)) return resultOut("Поле должно содержать только цифры");	
-	if(number.match(/[^01]+$/)) return resultOut("Введите число в двоично представлении");
+	return resultOut(_this, binaryNumber);
+}
+
+function binaryToDecimal(_this ,number){
 	numberLength = number.length;
 	index = 0;
 	power = numberLength-1;	
@@ -32,15 +21,28 @@ window.addEventListener("DOMContentLoaded", function(event) {
 		index++;
 		power--;
 	}
-	return resultOut(decimalNumber);
-  }
-  function resultOut(string) {
-	//return this.getElementsByClassName('result_out')[0].innerHTML = string;
-  }*/
-  
-  function getNumber (event){
-  	event.preventDefault();
-  	console.log(this);
-  }
+	return resultOut(_this, decimalNumber);
+}
 
+function resultOut(_this, string) {	
+	return _this.getElementsByClassName('result_out')[0].innerHTML = string;	
+}
+
+window.addEventListener("DOMContentLoaded", function(event) {
+  var forms = document.getElementsByClassName("form");
+  var formHandler = function formHandler(event) { 
+  	event.preventDefault();
+  	var _this = this;
+    resultOut(_this, '');
+    var number = _this.querySelector('input[name="number"]').value;
+    if(number == '') return resultOut(_this, "Поле ввода не должно быть пустым");
+	if(!number.match(/^\d+$/)) return resultOut(_this, "Поле должно содержать только цифры");
+	if(_this.id == "binaryToDecimal"){
+		if(number.match(/[^01]+$/)) return resultOut(_this, "Введите число в двоично представлении");
+	}
+	(_this.id == "binaryToDecimal") ? binaryToDecimal(_this, number) : decimalToBinary(_this, number);
+  };  
+  Object.keys(forms).forEach(function(key) {
+    forms[key].onsubmit = formHandler;    
+  });
 });
